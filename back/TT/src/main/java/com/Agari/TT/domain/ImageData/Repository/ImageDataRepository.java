0228bcf00e7a65aaa4c114173e4d7af7a3e04787
채승지 <cotmdwl1405@gmail.com>
2024-02-07 20:33:14 +0900

    chore : 데이터베이스 및 cloud 연결

---
+++ back/TT/src/main/java/com/Agari/TT/domain/ImageData/Repository/ImageDataRepository.java
@@ -0,0 +1,8 @@
+package com.Agari.TT.domain.ImageData.Repository;
+
+import com.Agari.TT.domain.ImageData.Entity.ImageData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageDataRepository extends JpaRepository<ImageData,Long> {
+
+}
