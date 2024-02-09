45dc263f00a05b67949ee82893771c02e631bb98
채승지 <cotmdwl1405@gmail.com>
2024-02-09 23:28:51 +0900

    feat : 쓰레기 업로드 시 코인 지급 기능 개발

--- back/TT/src/main/java/com/Agari/TT/domain/Trash/Controller/TrashController.java
+++ back/TT/src/main/java/com/Agari/TT/domain/Trash/Controller/TrashController.java
@@ -1,6 +1,7 @@
 package com.Agari.TT.domain.Trash.Controller;
 
 import com.Agari.TT.domain.Member.Entity.MemberDetail;
+import com.Agari.TT.domain.Response.ResponseService;
 import com.Agari.TT.domain.Response.SingleResponse;
 import com.Agari.TT.domain.Trash.Service.TrashService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +11,26 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
 public class TrashController {
 
     @Autowired
     TrashService trashService;
 
+    @Autowired
+    ResponseService responseService;
+
 
     /**
      * 쓰레기 사진 업로드
      */
-    public SingleResponse uploadTrash(@RequestPart MultipartFile trashImage, @AuthenticationPrincipal MemberDetail memberDetail){
+    public SingleResponse uploadTrash(@RequestPart MultipartFile trashImage,
+                                      @AuthenticationPrincipal MemberDetail memberDetail) throws IOException {
 
-        trashService.save(trashImage,memberDetail.getUsername());
+        int coin = trashService.save(trashImage,memberDetail.getUsername());
 
+        return responseService.getSingleResponse(coin);
     }
 }
--- back/TT/src/main/java/com/Agari/TT/domain/Trash/Service/TrashService.java
+++ back/TT/src/main/java/com/Agari/TT/domain/Trash/Service/TrashService.java
@@ -1,14 +1,54 @@
 package com.Agari.TT.domain.Trash.Service;
 
+import com.Agari.TT.domain.ImageData.Service.ImageService;
+import com.Agari.TT.domain.Member.Entity.Member;
+import com.Agari.TT.domain.Member.Repository.MemberRepository;
+import com.Agari.TT.domain.Member.Service.MemberService;
+import com.Agari.TT.domain.Trash.Entity.Trash;
+import com.Agari.TT.domain.Trash.Repository.TrashRepository;
+import com.Agari.TT.global.Exception.CustomErrorCode;
+import com.Agari.TT.global.Exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class TrashService {
 
-    public void save(MultipartFile trashImage, String loginId) {
+    private final ImageService imageService;
 
+    private final MemberRepository memberRepository;
+
+    private final TrashRepository trashRepository;
+
+
+    public int save(MultipartFile trashImage, String loginId) throws IOException {
+        String url = ImageService.exec(trashImage);
+
+        Member member =  memberRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new CustomException(CustomErrorCode.USER_NOT_FOUND));
+
+        // 여기서 이제 ai랑 연동하는데 내가 url만 주면 바로 사진 가져가서 분석이 가능한 지 (지금은 대충 받았다고 치겠음)
+
+        String trashType = "플라스틱";
+
+        Trash trash = Trash.builder()
+                .filePath(url)
+                .trashType(trashType)
+                .member(member)
+                .build();
+
+        // 쓰레기 타입에 따른 코인 차등 부여
+
+        trashRepository.save(trash);
+
+        memberRepository.updateMemberCoin(500,member);
+
+        return 500;
     }
 }
