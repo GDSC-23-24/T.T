791b86ec78fd4e6afc57c8967fa961e7f5e074b4
rzl0zi <program.yunji@gmail.com>
2024-02-10 20:27:31 +0900

    feat: SignUp.js개발

--- front/App.js
+++ front/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Welcome from './Screens/Sign/Welcome';
 import Login from './Screens/Sign/Login';
+import SignUp from './Screens/Sign/SignUp';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ export default function App() {
        
         <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
 
       </Stack.Navigator>
     </NavigationContainer>
