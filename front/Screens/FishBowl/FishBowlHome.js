012b320ea76bbe4c73eebd1428d9207f677b8611
rzl0zi <program.yunji@gmail.com>
2024-02-19 18:01:53 +0900

    feat: FishBowlHome 통신연결

--- front/Screens/FishBowl/FishBowlHome.js
+++ front/Screens/FishBowl/FishBowlHome.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity, ImageBackground, Image } from 'react-native';
 import BottomBar from '../Common/BottomBar';
 import TopBar from '../Common/TopBar';
-
+import { API_TOKEN } from '../../API';
 const FishBowlHome = () => {
+    const [userData, setUserData] = useState(null);
+    const [components, setComponents] = useState([]);
+
+    const componentImagePaths = {
+        'yellow fish': require('../../Asset/img/Yellowfish.png'),
+        'red fish': require('../../Asset/img/Redfish.png'),
+        'mint fish': require('../../Asset/img/Mintfish.png'),
+        'green fish': require('../../Asset/img/Greenfish.png'),
+
+    }
+    useEffect(() => {
+        fetchHomeData();
+    }, []);
+
+    const fetchHomeData = async () => {
+        try {
+            const response = await fetch('http://10.0.2.2:8080/api/fish-bowl/home', {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + API_TOKEN,
+                }
+            });
+            const data = await response.json();
+            if (data.success) {
+                setUserData(data.data);
+                setComponents(data.data.componentResponseDtoList);
+            }
+        } catch (error) {
+            console.error('Error fetching home data:', error);
+        }
+    };
+
     return (
         <ImageBackground source={require('../../Asset/img/background_bowl.png')} style={styles.backgroundImage}>
             <View style={styles.container}>
@@ -14,52 +47,52 @@ const FishBowlHome = () => {
 
                 <View style={styles.rowContainer}>
                     <View style={styles.column}>
+                        {/* Display user information */}
                         <Text style={styles.name}>Name</Text>
                         <View style={styles.info}>
-                        <Image source={require('../../Asset/img/badge.png')} style={styles.infoImg} />
-                        <Text style={styles.name1}>Salad Bowl</Text>
-                        </View>
-
-                        <Text style={styles.name}>Age</Text>
-                        <View style={styles.info}>
-                        <Image source={require('../../Asset/img/cake.png')} style={styles.infoImg} />
-                        <Text style={styles.name1}>1Month</Text>
+                            <Image source={require('../../Asset/img/badge.png')} style={styles.infoImg} />
+                            <Text style={styles.name1}>{userData && userData.memberDto.nickname}</Text>
                         </View>
 
                         <Text style={styles.name}>Visitor</Text>
                         <View style={styles.info}>
-                        <Image source={require('../../Asset/img/group.png')} style={styles.infoImg} />
-                        <Text style={styles.name1}>120.9K</Text>
+                            <Image source={require('../../Asset/img/group.png')} style={styles.infoImg} />
+                            <Text style={styles.name1}>{userData && userData.viewCount}</Text>
                         </View>
-
-
                     </View>
 
                     <View style={styles.column}>
-
+                        {/* Display additional information */}
                         <Text style={styles.name}>Like</Text>
                         <View style={styles.info}>
-                        <Image source={require('../../Asset/img/favorite.png')} style={styles.infoImg} />
-                        <Text style={styles.name1}>12.4K</Text>
+                            <Image source={require('../../Asset/img/favorite.png')} style={styles.infoImg} />
+                            <Text style={styles.name1}>{userData && userData.likesCount}</Text>
                         </View>
-                        
+
                         <Text style={styles.name}>My Point</Text>
                         <View style={styles.Frame121}>
                             <View style={styles.info}>
-                            <Image source={require('../../Asset/img/coin.png')} style={styles.infoImg} />
-                            <Text style={styles.name1}>5,300P</Text>
+                                <Image source={require('../../Asset/img/coin.png')} style={styles.infoImg} />
+                                <Text style={styles.name1}>{userData && userData.coin}P</Text>
                             </View>
                         </View>
-
-
                     </View>
                 </View>
                 <TouchableOpacity style={styles.Rectangle2225}>
                     <Image source={require('../../Asset/img/add.png')} style={styles.add} />
                 </TouchableOpacity>
-
+                <Image source={require('../../Asset/img/sand.png')} style={styles.sand} />
                 <View style={styles.bowlMain}>
                     <Image source={require('../../Asset/img/fishbowl0.png')} style={styles.fishbowl0} />
+                    {/* Display components in the fish bowl */}
+                    {components.map((component) => (
+                        <Image
+                            key={component.id}
+                            source={componentImagePaths[component.componentName]}
+                            style={{ ...styles.fishbowl0, position: 'absolute', top: component.y, left: component.x }}
+                            onError={(e) => console.log(`Error loading image: ${e.nativeEvent.error}`)}
+                        />
+                    ))}
                 </View>
             </View>
             {/* Bottom Bar */}
@@ -97,7 +130,7 @@ const styles = StyleSheet.create({
     column: {
         flex: 1,
         flexDirection: 'column',
-        marginLeft:20
+        marginLeft: 20,
     },
     Frame121: {
         width: 160,
@@ -107,7 +140,7 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         backgroundColor: '#fff',
         marginTop: 5,
-        alignItems:'center'
+        alignItems: 'center',
     },
     name: {
         fontSize: 15,
@@ -119,7 +152,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         color: '#1e1e1e',
-        marginLeft:5
+        marginLeft: 5,
     },
     infoImg: {
         width: 30,
@@ -149,13 +182,22 @@ const styles = StyleSheet.create({
     fishbowl0: {
         width: 300,
         height: 300,
-        position: 'absolute',
-        top: 40,
-        right: 35,
+        marginLeft: 30
     },
-    info:{
+    sand: {
+        position: 'absolute',
+        width: 250,
+        height: 250,
+        top: 320,
+        left:80
+    },
+    fishbowl1: {
+        width: 30,
+        height: 30,
+    },
+    info: {
         flexDirection: 'row',
-    }
+    },
 });
 
 export default FishBowlHome;
